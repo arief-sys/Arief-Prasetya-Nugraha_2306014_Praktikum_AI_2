d41c81f83e37ff510bcdded187c97837160b40ea
arief-sys <arief.study@gmail.com>
2025-03-22 12:08:54 +0700

    Update bigger than.pl

--- First_Order_Logic/bigger than.pl
+++ First_Order_Logic/bigger than.pl
@@ -4,4 +4,4 @@ bigger(keledai, anjing).
 bigger(keledai, monyet).
 
 is_bigger(X, Y) :- bigger(X, Y).
-is_bigger(X, Y) :- bigger(X, Z), is_bigger(Z,Y).is_
+is_bigger(X, Y) :- bigger(X, Z), is_bigger(Z,Y).
